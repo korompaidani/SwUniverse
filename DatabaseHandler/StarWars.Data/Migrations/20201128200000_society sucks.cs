affaf7a3ab1245a8c9bba4a19b33ad3299d6e10f
Dániel Korompai <korompaidani@gmail.com>
2020-11-28 21:07:26 +0100

    Society missmatch.... with sql

---
+++ DatabaseHandler/StarWars.Data/Migrations/20201128200000_society sucks.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StarWars.Data.Migrations
+{
+    public partial class societysucks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "History4",
+                table: "Society",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "History4",
+                table: "Society");
+        }
+    }
+}
---
+++ DatabaseHandler/StarWars.Data/Migrations/20201128200142_society delete histoy4.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StarWars.Data.Migrations
+{
+    public partial class societydeletehistoy4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "History4",
+                table: "Society");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "History4",
+                table: "Society",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
